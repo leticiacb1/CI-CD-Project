b8e525479b4b8deec317efde0c680eb700e872b0
leticiacb1 <leticiacb1@al.insper.edu.br>
2024-09-24 16:47:39 -0300

    fix: adjusting source code to handle null input.

--- src/word_count.py
+++ src/word_count.py
@@ -3,11 +3,18 @@ def word_count_handler(event, context):
     A simple function that counts
     the number of words in a string
     """
-    msg = event["body"]
 
-    n_words = len(msg.split())
+    if ((not event) or (event.get('body') == None)):
+        return {"error": "no body"}
 
-    return {
-        "len": len(msg),
-        "words": n_words,
-    }
+    try:
+        msg = event["body"]
+        n_words = len(msg.split())
+
+        return {
+            "len": len(msg),
+            "words": n_words,
+        }
+    except Exception as e:
+        return {"error": e}
+    
